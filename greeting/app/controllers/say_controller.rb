03810245436867d2fd676cf79c2e06d5b60f50d9
Sia <siakaramalegos@gmail.com>
2015-08-11 08:47:30 -0500

    Added current day and time using stamp gem

--- greeting/app/controllers/say_controller.rb
+++ greeting/app/controllers/say_controller.rb
@@ -1,6 +1,8 @@
 class SayController < ApplicationController
   def hello
-    @time = Time.now
+    right_now = Time.now
+    @current_time = right_now.stamp("9:00 pm")
+    @current_date = right_now.stamp("February 1, 1999")
   end
 
   def goodbye
