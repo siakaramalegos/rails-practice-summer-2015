03029eacc9d0053811bd94aa27231538a2ea1bae
Sia <siakaramalegos@gmail.com>
2015-08-12 10:02:14 -0500

    Updates views to show owners

--- kitty_daycare/app/controllers/cats_controller.rb
+++ kitty_daycare/app/controllers/cats_controller.rb
@@ -69,6 +69,6 @@ class CatsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cat_params
-      params.require(:cat).permit(:name, :breed, :age, :owner_name)
+      params.require(:cat).permit(:name, :breed, :age, :owner_id)
     end
 end
