76d26a751221881754b487e9df6f3b8b733e60db
Sia <siakaramalegos@gmail.com>
2015-08-13 10:56:03 -0500

    Added validation

--- music_db/app/models/genre.rb
+++ music_db/app/models/genre.rb
@@ -1,3 +1,24 @@
 class Genre < ActiveRecord::Base
   has_many :artists
+
+  validates :name, presence: true, length: {minimum: 3}, uniqueness: true
+
+  # acceptance... validates :terms_of_service, acceptance: true
+  #if/unless...
+    # validates :card_number, presence: true, if: :paid_with_card?
+
+    # def paid_with_card?
+    #   payment_type == "card"
+    # end
+
 end
+
+# == Schema Information
+#
+# Table name: genres
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- music_db/app/models/song.rb
+++ music_db/app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  validates :name, :artist_id, presence: true
+  validates_uniqueness_of :name, :scope => :artist_id
+
+  scope :recent, -> (minutes_past = 60)
+  {where("created_at > ?", minutes_past.minutes.ago)}
 end
+
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  artist_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+# pretend we also have description, rating
+# Indexes
+#
+#  index_songs_on_artist_id  (artist_id)
+#
